3f64d038b35b9d9dd8226b71444d72ecedb1a3f1
PabloCalleja <pablocallejaibanez@gmail.com>
2018-10-11 12:22:18 +0200

    Now Logs

--- src/main/java/oeg/easytv/annotator/webservice/ProjectController.java
+++ src/main/java/oeg/easytv/annotator/webservice/ProjectController.java
@@ -7,12 +7,7 @@
 
 package oeg.easytv.annotator.webservice;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
